8c6c8832a125f2fb66ee786ce3da4e87446228cd
BuildTools <unconfigured@null.spigotmc.org>
2023-05-20 17:47:16 +0200

    [feat] rewriting into a plugin/pipelined architecture

--- index.js
+++ index.js
@@ -1,99 +1,118 @@
 const fs = require('fs');
 const showdown  = require('showdown');
-const statik = require('reload-static');
 
-const outDir = "./out/";
+const plugins = {
+    "Markdown2Html": {
+        init: initMarkdown2Html,
+        apply: applyMarkdown2Html
+    },
+    "ApplyPugTemplate": {},
+    "WriteFile":{
+        init: initWriteFile,
+        apply: applyWriteFile
+    },
+    "Aggregate":{},
+    "CompileSass":{}
+}
 
-const postsDir = "./posts/";
-const staticDir = "./static/";
+/// Markdown2Html Plugin Begin
+var mdConverter = new showdown.Converter();
+function initMarkdown2Html(pluginSettings) {
+    var pluginContext = {};
+    pluginContext.converter = mdConverter;
+    return pluginContext;
+}
 
-fs.watch(postsDir, (eventType, filename) => {
-    gen();
-});
+function applyMarkdown2Html(markdownText, itemConfig, pluginContext){
+    return pluginContext.converter.makeHtml(markdownText);
+}
+/// Markdown2Html Plugin End
 
-fs.watch(staticDir, (eventType, filename) => {
-    gen();
-});
+/// WriteFile Plugin Begin
+function initWriteFile(pluginSettings){
+    var pluginContext = {};
+    pluginContext.location = pluginSettings.location;
 
-gen();
-
-const file = new statik.Server(outDir);
-const server = require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
-}).listen(8080);
-
-file.setReloadable(server);
-
-function gen() {
-    console.log("Generating...");
-    if (!fs.existsSync(outDir)){
-        fs.mkdirSync(outDir);
+    if(pluginSettings.extension) {
+        pluginContext.strategy = "writeWithExtension";
+        pluginContext.extension = pluginSettings.extension;
     }
 
-    const posts = JSON.parse(fs.readFileSync(postsDir + "config.json"));
-    const otherFiles = JSON.parse(fs.readFileSync(staticDir + "config.json"));
-
-    var index = header({title: "Index"});
-    index = index + `<h1>Kev The Dev The Blog!</h1>`;
-
-    converter = new showdown.Converter();
-    for (const post of posts) {
-        const text = fs.readFileSync(postsDir + post.file, 'utf8');
-        const html = postHeader(post) + converter.makeHtml(text) + postFooter(post);
-        const fileName = titleToHtmlFileName(post);
-        fs.writeFileSync(outDir + fileName, html);
-        index = index + `
-            <a href="/${fileName}">
-                <div class="blog-box blog-info-box">
-                    <h3>${post.title}</h3>
-                    <p>${post.summary}
-                </div>
-            </a>`;
+    if(pluginSettings.name) {
+        pluginContext.strategy = "writeWithName";
+        pluginContext.fileName = pluginSettings.name;
     }
 
-    index = index + footer();
-    fs.writeFileSync(outDir + "index.html", index);
+    if(!pluginContext.strategy) {
+        throw new Error("No valid file writing strategy found! Please provide either a file name or extension");
+    }
+    return pluginContext;
+}
 
-    for (const file of otherFiles) {
-        fs.copyFileSync(staticDir + file.file, outDir + file.destName);
+function applyWriteFile(fileText, itemConfig, pluginContext){
+    var fileName = pluginContext.location + "/";
+
+    if(pluginContext.strategy === "writeWithExtension") {
+        fileName = fileName + itemConfig.fileName + "." + pluginContext.extension;
+    } else if(pluginContext.strategy === "writeWithName") {
+        fileName = fileName + pluginContext.fileName;
+    }
+
+    fs.writeFileSync(fileName, fileText);
+    return fileText;
+}
+/// WriteFile Plugin End
+
+const testPipeline = {
+    "name": "genPosts",
+    "fileMatcher": ".*\\.md$",
+    "pipeline": [{
+        "plugin": "Markdown2Html"
+    },{
+        "plugin": "WriteFile",
+        "pluginSettings": {
+            "location": "./out",
+            "extension": "html"
+        }
+    }]
+};
+const folder = "betterPosts/";
+
+///Pipeline Execution Engine Begin
+function buildPipeline(pipelineConfig) {
+    var pipeline = { stages: []};
+    for(const config of pipelineConfig) {
+        var plugin = plugins[config.plugin];
+        var pluginConfig = plugin.init(config.pluginSettings);
+        pipeline.stages.push({ plugin: plugin, config: pluginConfig });
+    }
+    return pipeline;
+}
+
+function executePipeline(inputs, pipeline) {
+    var currents = inputs;
+
+    for(const stage of pipeline.stages) {
+        var nexts = [];
+        for(const item of currents) {
+            var next = {
+                itemConfig: item.itemConfig,
+                text: stage.plugin.apply(item.text, item.itemConfig, stage.config)
+            };
+            nexts.push(next);
+        }
+        currents = nexts;
     }
 }
+///Pipeline Execution Engine End
 
-function titleToHtmlFileName(post) {
-    var title = post.title;
-    title = title.replace(" ", "_");
-    title = title.replace(/[^a-zA-Z_]/g, "");
-    return title + ".html";
-}
+var test = buildPipeline(testPipeline.pipeline);
 
-function postHeader(post) {
-    return header(post) + `
-        <h1>${post.title}</h1>
-        <div class="blog-box">
-`
-}
+var inp = [{
+    text: "## Hello World!",
+    itemConfig: {
+        fileName: "hello"
+    }
+}];
 
-function postFooter(post) {
-    return `
-    </div>
-    ` + footer(post);
-}
-
-function header(post) {
-    return `<!DOCTYPE html>
-<html>
-<head>
-<title>Kev's blog - ${post.title}</title>
-<link rel="stylesheet" href="blogStyle.css">
-</head>
-<body>
-`
-}
-
-function footer(post) {
-    return `
-</body>
-</html>`
-}
+executePipeline(inp, test);
